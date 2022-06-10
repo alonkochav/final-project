5f4d60eda2e820cb40ced9ba663967f6d6537149
alonkochav <alonkochav@gmail.com>
2022-06-10 13:11:45 +0300

    driver init -> pageobjects

--- src/main/java/utilities/Base.java
+++ src/main/java/utilities/Base.java
@@ -1,4 +1,9 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
+
 public class Base {
+
+    protected static WebDriver driver;
+
 }
--- src/main/java/utilities/CommonOps.java
+++ src/main/java/utilities/CommonOps.java
@@ -1,4 +1,65 @@
 package utilities;
 
-public class CommonOps {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonOps extends Base {
+    public void initBrowser(String browserType) {
+        if (browserType.equalsIgnoreCase("chrome"))
+            driver = initChromeDriver();
+        else if (browserType.equalsIgnoreCase("firefox"))
+            driver = initFirefoxDriver();
+        else if (browserType.equalsIgnoreCase("ie/edge"))
+            driver = initIEDriver();
+        else
+            throw new RuntimeException("Invalid browser type");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://localhost:3000/");
+        ManagePages.initGrafana();
+    }
+
+    public static WebDriver initChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        return driver;
+    }
+
+    public static WebDriver initFirefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public static WebDriver initIEDriver() {
+        WebDriverManager.iedriver().setup();
+        driver = new InternetExplorerDriver();
+        return driver;
+    }
+
+    @BeforeClass
+    public void startSession() {
+        String platform = "web";
+        if (platform.equalsIgnoreCase("web"))
+            initBrowser("chrome");
+//        else if (platform.equalsIgnoreCase("mobile"))
+//            initMobile();
+        else
+            throw new RuntimeException("Invalid platform name");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public void closeSession() {
+        driver.quit();
+
+    }
 }
+
+
