01b30615440ab7d0f16336b0632c151748350556
alonkochav <alonkochav@gmail.com>
2022-07-10 15:49:21 +0300

    Mobile Flow -->  Mobile Test Case

--- src/main/java/workflows/MobileFlows.java
+++ src/main/java/workflows/MobileFlows.java
@@ -1,9 +1,11 @@
 package workflows;
 
 import extensions.MobileActions;
+import io.qameta.allure.Step;
 
 public class MobileFlows extends utilities.CommonOps {
 
+    @Step("Business Flow : Fill Form And Calculate Mortgage")
     public static void calculateMortgage(String amount, String term, String rate){
         MobileActions.updateText(mortgageMain.txt_amount, amount);
         MobileActions.updateText(mortgageMain.txt_term, term);
