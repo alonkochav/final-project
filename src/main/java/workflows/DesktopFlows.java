ef48b70659fe06865fa962fd11b1fc3edca0362a
alonkochav <alonkochav@gmail.com>
2022-10-11 17:38:02 +0300

    Desktop Calculator - int only refactoring

--- src/main/java/workflows/DesktopFlows.java
+++ src/main/java/workflows/DesktopFlows.java
@@ -2,6 +2,7 @@ package workflows;
 
 import extensions.UIActions;
 import io.qameta.allure.Step;
+import org.python.antlr.ast.Num;
 import utilities.CommonOps;
 import utilities.Log;
 
@@ -9,16 +10,68 @@ import java.util.LinkedList;
 
 public class DesktopFlows extends CommonOps {
 
+    @Step("Calculate input operation of two numbers: a ? b ")
+    public static void calculateAny(int a, char o, int b) {
+        UIActions.click(calcMain.getBtnClear());
 
+        if (getDigits(a).size() == 1) {
+            getCalcButtonObjects(a);
+        } else {
+            getMultipleDigits(a);
+        }
+        getCalcButtonOperations(o);
 
+        if (getDigits(b).size() == 1) {
+            getCalcButtonObjects(b);
+        } else {
+            getMultipleDigits(b);
+        }
+
+        UIActions.click(calcMain.getBtnEquals());
+
+        Log.info(calcMain.getFieldResult().getText());
+    }
+
+    @Step("Calculate Addition of two numbers: a + b ")
+    public static void calculateAddition(int a, int b) {
+        Log.info("Integer a is " + a);
+        Log.info("Integer b is "+ b);
+        calculateAny(a,'+',b);
+    }
+
+    @Step("Calculate Subtraction of two numbers: a - b ")
+    public static void calculateSubtraction(int a, int b) {
+        Log.info("Integer a is " + a);
+        Log.info("Integer b is "+ b);
+        calculateAny(a,'-',b);
+
+    }
+
+    @Step("Calculate Multiplication of two numbers: a * b ")
+    public static void calculateMultiplication(int a, int b) {
+        Log.info("Integer a is " + a);
+        Log.info("Integer b is "+ b);
+        calculateAny(a,'*',b);
+
+    }
+
+    @Step("Calculate Division of two numbers: a / b ")
+     public static void calculateDivision(int a, int b) {
+        Log.info("Integer a is " + a);
+        Log.info("Integer b is "+ b);
+        calculateAny(a,'/',b);
+
+    }
+
+    @Step("Retrieve number digit and click relative button")
     public static void getCalcButtonObjects(int n) {
         switch (n) {
             case 0:
                 UIActions.click(calcMain.getBtnZero());
-            break;
+                break;
             case 1:
                 UIActions.click(calcMain.getBtnOne());
-            break;
+                break;
             case 2:
                 UIActions.click(calcMain.getBtnTwo());
                 break;
@@ -43,13 +96,17 @@ public class DesktopFlows extends CommonOps {
             case 9:
                 UIActions.click(calcMain.getBtnNine());
                 break;
-
-            // Default case
+            case '.':
+                UIActions.click(calcMain.getBtnPoint());
+                break;
+                // Default case
             default:
                 Log.info("Not found");
+                break;
         }
     }
 
+    @Step("Retrieve operation character and click relative button")
     public static void getCalcButtonOperations(char o) {
         switch (o) {
             case '+':
@@ -63,80 +120,50 @@ public class DesktopFlows extends CommonOps {
                 break;
             case '/':
                 UIActions.click(calcMain.getBtnDivide());
+                break;
+
+            // Default case
+            default:
+                Log.info("Operation Not found");
         }
     }
 
+    @Step("Check if character is Numeric digit")
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+
+    @Step("Retrieve multiple digits as stack of input number")
     public static LinkedList<Integer> getDigits(int n){
         LinkedList<Integer> stack = new LinkedList<>();
-        while (n > 0) {
-            stack.push( n % 10 );
+
+        System.out.println(isNumeric(Integer.toString(n)));
+
+        while (n > 0){
+            stack.push(n % 10);
             n = n / 10;
         }
-        return stack;
+         return stack;
     }
 
-    public static void getMultipleDigits(int n){
+    @Step("Create and pop stack of the LinkedList of multiple digits")
+    public static LinkedList<Integer>  getMultipleDigits(int n){
         LinkedList<Integer> stack = getDigits(n);
+
         while (!stack.isEmpty()){
             getCalcButtonObjects(stack.getFirst());
             stack.pop();
+
         }
-    }
-
-    public static void calculateAny(int a, char o, int b) {
-        UIActions.click(calcMain.getBtnClear());
-        if (getDigits(a).size() == 1) {
-            getCalcButtonObjects(a);
-        } else {
-            getMultipleDigits(a);
-        }
-
-        getCalcButtonOperations(o);
-
-        if (getDigits(b).size() == 1) {
-            getCalcButtonObjects(b);
-        } else {
-            getMultipleDigits(b);
-        }
-
-        UIActions.click(calcMain.getBtnEquals());
-        Log.info(calcMain.getFieldResult().getText());
+        return stack;
 
     }
 
-    @Step("Calculate Addition of two numbers: a + b ")
-    public static void calculateAddition(int a, int b) {
-        Log.info("Number a is " + a);
-        Log.info("Number b is "+ b);
-
-        calculateAny(a,'+',b);
-    }
-
-    @Step("Calculate Subtraction of two numbers: a - b ")
-    public static void calculateSubtraction(int a, int b) {
-        Log.info("Number a is " + a);
-        Log.info("Number b is "+ b);
-
-        calculateAny(a,'-',b);
-
-    }
-
-    @Step("Calculate Multiplication of two numbers: a * b ")
-    public static void calculateMultiplication(int a, int b) {
-        Log.info("Number a is " + a);
-        Log.info("Number b is "+ b);
-
-        calculateAny(a,'*',b);
-
-    }
-
-    @Step("Calculate Division of two numbers: a / b ")
-    public static void calculateDivision(int a, int b) {
-        Log.info("Number a is " + a);
-        Log.info("Number b is "+ b);
-
-        calculateAny(a,'/',b);
-
-    }
 
 }
--- src/test/java/sanity/TestCalculatorDesktop.java
+++ src/test/java/sanity/TestCalculatorDesktop.java
@@ -1,6 +1,5 @@
 package sanity;
 
-import com.google.common.util.concurrent.Uninterruptibles;
 import extensions.Verifications;
 import io.qameta.allure.Description;
 import org.testng.annotations.Listeners;
@@ -9,58 +8,61 @@ import utilities.CommonOps;
 import workflows.DesktopFlows;
 
 import java.text.NumberFormat;
-import java.util.concurrent.TimeUnit;
 
 
 @Listeners(utilities.Listeners.class)
 public class TestCalculatorDesktop extends CommonOps {
 
-    @Test(description = "Test 01 - Verify Addition Operation"  )//, enabled=false)
+    @Test(description = "Test 01 - Verify Addition Operation"
+//    , enabled = false
+    )
     @Description("This Test Verifies the Addition Operation")
-    public void test01_verifyAdditionCommand(){
+    public void test01_verifyAdditionOperation() {
         int a = 833;
-        char o = '+';
         int b = 12;
         int res = a + b;
         String exp = NumberFormat.getIntegerInstance().format(res);
 
-        DesktopFlows.calculateAny(a, o, b);
-        Verifications.verifyTextInElement(calcMain.getFieldResult(),"Display is "+ exp );
+        DesktopFlows.calculateAddition(a, b);
+        Verifications.verifyTextInElement(calcMain.getFieldResult(), "Display is " + exp);
     }
 
-    @Test(description = "Test 02 - Verify Subtraction Operation"  )//, enabled=false)
+    @Test(description = "Test 02 - Verify Subtraction Operation"
+//    , enabled=false
+    )
     @Description("This Test Verifies the Subtraction Operation")
-    public void test02_verifySubtractionOperation(){
-        int a = 8133;
-        char o = '-';
-        int b = 123;
+    public void test02_verifySubtractionOperation() {
+        int a = 9;
+        int b = 5;
         int res = a - b;
         String exp = NumberFormat.getIntegerInstance().format(res);
         DesktopFlows.calculateSubtraction(a,b);
-        Verifications.verifyTextInElement(calcMain.getFieldResult(),"Display is "+ exp );
+        Verifications.verifyTextInElement(calcMain.getFieldResult(), "Display is " + exp);
     }
 
-    @Test(description = "Test 03 - Verify Multiplication Operation" )//, enabled=false)
+    @Test(description = "Test 03 - Verify Multiplication Operation"
+//    , enabled=false
+    )
     @Description("This Test Verifies the Multiplication Operation")
-    public void test03_verifyMultiplicationOperation(){
-        int a = 83;
-        char o = '*';
-        int b = 13;
+    public void test03_verifyMultiplicationOperation() {
+        int a = 10;
+        int b = 2;
         int res = a * b;
         String exp = NumberFormat.getIntegerInstance().format(res);
         DesktopFlows.calculateMultiplication(a,b);
-        Verifications.verifyTextInElement(calcMain.getFieldResult(),"Display is "+ exp );
+        Verifications.verifyTextInElement(calcMain.getFieldResult(), "Display is " + exp);
     }
 
-    @Test(description = "Test 04 - Verify Division Operation"  )//, enabled=false)
+    @Test(description = "Test 04 - Verify Division Operation"
+//    , enabled=false
+    )
     @Description("This Test Verifies the Division Operation")
     public void test04_verifyDivisionOperation(){
         int a = 144;
-        char o = '/';
         int b = 12;
         int res = a / b;
         String exp = NumberFormat.getIntegerInstance().format(res);
-        DesktopFlows.calculateDivision(a,b);
+        DesktopFlows.calculateDivision(a, b);
         Verifications.verifyTextInElement(calcMain.getFieldResult(),"Display is "+ exp );
     }
 }
